851536fbe0cd10491485e062809dcace13679a81
Anastasia Shyshenok <shyshenoka@gmail.com>
2017-03-01 22:34:13 +0200

    Refactoring to OOP

--- myscript.js
+++ myscript.js
@@ -1,7 +1,10 @@
 var canvas = document.getElementById("myCanvas");
 
 var ctx = canvas.getContext("2d");
+
 var ballRadius = 10;
+var heightRect = 15;
+var widthRect = 20;
 
 function Vector(x, y) {
     this.x = x;
@@ -13,71 +16,75 @@ Vector.prototype.add = function(v) {
     this.y += v.y;
 }
 
-
-function Ball(position, speed, radius, color) {
+function MoveableObject(position,speed) {
     this.position = position;
     this.speed = speed;
-    this.ballRadius = radius;
-    this.colorBall = color;
 }
 
+MoveableObject.prototype.draw = function(context) {
+    throw "Method draw is not implemented";
+}
 
+MoveableObject.prototype.step = function(width, height) {
+    throw "Method step is not implemented";
+}
+
+function ColorMoveableObject(position, speed, color) {
+    MoveableObject.apply(this, arguments);
+    this.color = color;
+}
+
+ColorMoveableObject.prototype = Object.create(MoveableObject.prototype);
+ColorMoveableObject.prototype.constructor = ColorMoveableObject;
+
+function Ball(position, speed, color, radius) {
+    ColorMoveableObject.apply(this, arguments);
+    this.radius = radius;
+}
+
+Ball.prototype = Object.create(ColorMoveableObject.prototype);
+Ball.prototype.constructor = Ball;
 
 Ball.prototype.draw = function(context) {
     context.beginPath();
-    context.arc(this.position.x, this.position.y, this.ballRadius, 0, Math.PI*2);
-    context.fillStyle = this.colorBall;
+    context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI*2);
+    context.fillStyle = this.color;
     context.fill();
     context.closePath();
 }
 
 Ball.prototype.step = function(width, height) {
-    if(this.position.x + this.speed.x > width - this.ballRadius || this.position.x + this.speed.x < this.ballRadius) {
+    if(this.position.x + this.speed.x > width - this.radius || this.position.x + this.speed.x < this.radius) {
         this.speed.x = -this.speed.x;
     }
-    if(this.position.y + this.speed.y > height - this.ballRadius || this.position.y + this.speed.y < this.ballRadius) {
+    if(this.position.y + this.speed.y > height - this.radius || this.position.y + this.speed.y < this.radius) {
         this.speed.y = -this.speed.y;
     }
     
     this.position.add(this.speed);
-
 }
+
 
 var firstBall = new Ball(
     new Vector(canvas.width/2, canvas.height-30),
     new Vector(2, -2),
-    ballRadius,
-    "#00ff00"
+    "#00ff00",
+    ballRadius
 );
 
-var secondBall = new Ball(
-    new Vector(canvas.width/2, canvas.height/23),
-    new Vector(4, -4), 
-    ballRadius * 2,
-    "#ff0000"
-);
-
-var thirdBall = new Ball(
-    new Vector(canvas.width/2, canvas.height/2),
-    new Vector(2.5, -2.5),
-    ballRadius *1.5,
-    "#0000FF"
-);
 
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-
     // draw balls
     firstBall.draw(ctx);
-    secondBall.draw(ctx);
-    thirdBall.draw(ctx);
+    console.log(firstBall);
+
 
     // update balls' position
     firstBall.step(canvas.width, canvas.height);
-    secondBall.step(canvas.width, canvas.height);
-    thirdBall.step(canvas.width, canvas.height)
+
 
 }
 
